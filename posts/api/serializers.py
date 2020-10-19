77f50e58dc78f1e91ab7c24cc1f40eea5a318ed0
88Ocelot <stas.mitrofanov1337@gmail.com>
2020-10-19 21:42:25 +0300

    posts refactoring

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,9 +1,6 @@
 from rest_framework import serializers
 
-from comments.api.serializers import CommentListSerializers
 from posts.models import Post
-from upvotes.api.serializers import UpvoteSerializer
-
 
 
 class PostDetailSerializer(serializers.ModelSerializer):
@@ -11,21 +8,32 @@ class PostDetailSerializer(serializers.ModelSerializer):
     comments = serializers.HyperlinkedIdentityField(view_name='posts-comments-list', read_only=True)
     upvotes = serializers.HyperlinkedIdentityField(view_name='posts-upvote-list', read_only=True)
     upvote_url = serializers.HyperlinkedIdentityField(view_name='posts-upvote', read_only=True)
+
     class Meta:
         model = Post
-        fields = ('title', 'body','author','comments','upvotes', 'upvote_url','created_at','updated_at',)
+        fields = ('title', 'body', 'author', 'comments', 'upvotes', 'upvote_url', 'created_at', 'updated_at',)
+
+
+class PostUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('title', 'body',)
 
 
 class PostListSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField()
     url = serializers.HyperlinkedIdentityField(view_name='posts-detail')
+
     class Meta:
         model = Post
         fields = ('title', 'body', 'author', 'created_at', 'get_upvotes', 'url')
 
 
 class PostCreateSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField()
+    author = serializers.CharField(default=serializers.CurrentUserDefault())
+
+
+
     class Meta:
         model = Post
         fields = ('title', 'body', 'author',)
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets,permissions
 
 from comments.mixins import CommentMixin
-from hoker_newz.mixins import SerializerClassesMixin
+from hoker_newz.mixins import SerializerClassesMixin, PermissionClassesMixin
+from hoker_newz.permissions import IsOwner
 from posts.api import serializers
 from posts.models import Post
 from upvotes.mixins import UpvoteMixin
@@ -10,8 +11,20 @@ from upvotes.mixins import UpvoteMixin
 class PostView(CommentMixin,
                UpvoteMixin,
                SerializerClassesMixin,
+               PermissionClassesMixin,
                viewsets.ModelViewSet):
+    permission_classes = {
+        'create': (permissions.IsAuthenticated,),
+        'list': (permissions.AllowAny,),
+        'update': (IsOwner,),
+        'default': (permissions.IsAuthenticated,),
+        'retrieve': (permissions.AllowAny,),
+        'comments_list': (permissions.AllowAny,),
+
+    }
+
     serializer_classes = {'default': serializers.PostDetailSerializer,
                           'list': serializers.PostListSerializer,
-                          'create': serializers.PostCreateSerializer, }
+                          'create': serializers.PostCreateSerializer,
+                          'update': serializers.PostUpdateSerializer,}
     queryset = Post.objects.all()
