e58f625ef45634364a4d06ea6d5ececd1f181e7e
88Ocelot <stas.mitrofanov1337@gmail.com>
2020-10-19 15:24:16 +0300

    commnts app

--- comments/api/serializers.py
+++ comments/api/serializers.py
@@ -1,11 +1,32 @@
+from django.urls import NoReverseMatch
 from rest_framework import serializers
+from rest_framework.relations import HyperlinkedIdentityField
+from rest_framework.reverse import reverse
 
 from comments.models import Comment
 
-
 class CommentListSerializers(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+
+
 
     class Meta:
         model = Comment
         fields = ('user', 'body', 'created_at', 'updated_at',)
-        read_only_fields =('user', 'body', 'created_at', 'updated_at',)
+        read_only_fields = ('user', 'body', 'created_at', 'updated_at',)
+
+
+class CommentCreateSerializers(serializers.ModelSerializer):
+    body = serializers.CharField(required=True)
+
+    class Meta:
+        model = Comment
+        fields = ('user', 'body',)
+
+
+class CommentSerializers(serializers.ModelSerializer):
+    body = serializers.CharField(required=True)
+
+    class Meta:
+        model = Comment
+        fields = ('body',)
---
+++ comments/mixins.py
@@ -0,0 +1,46 @@
+from rest_framework import decorators, status
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+
+from comments.api.serializers import CommentCreateSerializers, CommentListSerializers, CommentSerializers
+from comments.models import Comment
+
+
+class CommentMixin:
+    """
+    Mixin for adding comment function to viewset
+    """
+    comments_serializers = {'comment': CommentCreateSerializers,
+                            'comments_list': CommentListSerializers,
+                            'comment_update': CommentSerializers}
+
+    @decorators.action(url_path='comment', detail='comment-create', methods=('POST',), )
+    def comment(self, request, pk):
+        serializer = self.get_serializer_class()(data=request.data)
+        if serializer.is_valid():
+            comment = Comment.objects.create(**serializer.validated_data, post_id=pk)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @decorators.action(url_path='comments', detail='comments-list', methods=('GET',), )
+    def comments_list(self, request, pk):
+        comments = Comment.objects.filter(post_id=pk)
+        serializer = self.get_serializer_class()(comments, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    @decorators.action(url_path='comments/(?P<comment_id>\d+)', detail='comment-detail', methods=('PUT', 'PATCH',), )
+    def comment_update(self, request, pk, comment_id):
+        queryset = Comment.objects.all()
+        obj = get_object_or_404(queryset, id=comment_id, post=pk)
+        serializer = self.get_serializer_class()(obj, data=request.data, partial=True)
+        if serializer.is_valid():
+            print(serializer.validated_data)
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        if self.action not in self.comments_serializers.keys():
+            return super(CommentMixin, self).get_serializer_class()
+        else:
+            return self.comments_serializers.get(self.action)
