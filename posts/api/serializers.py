e9128249cfc29910718ddd37228eca2e94169a85
88Ocelot <stas.mitrofanov1337@gmail.com>
2020-10-19 12:19:24 +0300

    added comments to post serializer

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from comments.api.serializers import CommentListSerializers
 from posts.models import Post
 from upvotes.api.serializers import UpvoteSerializer
 
@@ -8,9 +9,10 @@ from upvotes.api.serializers import UpvoteSerializer
 class PostDetailSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField()
     upvotes = UpvoteSerializer(many=True, read_only=True)
+    comments = CommentListSerializers(many=True, read_only=True)
     class Meta:
         model = Post
-        fields = ('title', 'body','author', 'upvotes')
+        fields = ('title', 'body','author', 'upvotes', 'comments')
         read_only_fields = ('id', 'author', 'created_at', 'updated_at', 'get_upvotes')
 
 
