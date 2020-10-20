0f84d80ea43392937dae9c56e93eb8f371cc49fc
88Ocelot <stas.mitrofanov1337@gmail.com>
2020-10-20 21:58:30 +0300

    fixed static

--- hoker_newz/settings/prod.py
+++ hoker_newz/settings/prod.py
@@ -7,3 +7,5 @@ django_heroku.settings(locals())
 CELERY_BROKER_URL = os.environ.get("REDIS_URL")
 CELERY_RESULT_BACKEND = os.environ.get("REDIS_URL")
 DEBUG = False
+STATIC_ROOT = os.path.join(os.path.dirname( __file__ ), '..', 'static')
+STATIC_URL = "/static/"
--- hoker_newz/urls.py
+++ hoker_newz/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
@@ -21,8 +23,10 @@ from rest_framework import permissions
 from drf_yasg2.views import get_schema_view
 from drf_yasg2 import openapi
 
-from hoker_newz.settings import base as settings
-
+if os.environ.get('ENV', 'LOCAL')=='LOCAL'
+    from hoker_newz.settings import local as settings
+else:
+    from hoker_newz.settings import prod as settings
 schema_view = get_schema_view(
     openapi.Info(
         title="Snippets API",
