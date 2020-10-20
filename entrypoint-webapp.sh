120af9e4c064eab55ccedfd8e4b9578c8545770a
88Ocelot <stas.mitrofanov1337@gmail.com>
2020-10-20 16:32:16 +0300

    collect static hide

--- entrypoint-webapp.sh
+++ entrypoint-webapp.sh
@@ -3,5 +3,5 @@ sleep 5
 cd ${PROJECT_DIR}/
 python manage.py makemigrations blog
 python manage.py migrate
-python manage.py collectstatic --noinput --clear
+#python manage.py collectstatic --noinput --clear
 gunicorn --pythonpath '${PROJECT_DIR}/' hoker_newz.wsgi:application --reload -w 2 -b  0.0.0.0:8000
