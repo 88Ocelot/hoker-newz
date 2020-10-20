cdf88c353684c99b60044c6095018e2194e084bc
88Ocelot <stas.mitrofanov1337@gmail.com>
2020-10-20 21:40:40 +0300

    added logging

--- hoker_newz/settings/base.py
+++ hoker_newz/settings/base.py
@@ -24,10 +24,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-
-
-# Application definition
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 
 INSTALLED_APPS = [
     "django.contrib.admin",
